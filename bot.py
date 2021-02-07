58267bd4cfa1813aa3206ed46937483f1e785ec4
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2021-02-07 16:22:53 +0900

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+TOKEN = os.environ.get("DISCORD_TOKEN_HONMA")
+
+client = discord.Client()
+
+# 起動時の動作
+@client.event
+async def on_ready():
+    print('本間課長が出社しました')
+
+# メッセージ受信時の処理
+@client.event
+async def on_message(message):
+    # 送信者がBotだったら無視
+    if message.author.bot:
+        return
+    if message.content == '!hi':
+        await message.channel.send('課長と恋の実験室')
+
+client.run(TOKEN)
